b3535c6d8a1db7f9f70d9c32b48f8e90b4fecac3
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-10-30 12:13:11 +0300

    8. Render the Gift List in the App

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -12,3 +12,13 @@ it('renders correctly', () => {
     expect(app).toMatchSnapshot();
 });
 
+it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
+});
+
