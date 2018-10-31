42d9638f6f374c24522d776b345d00416e3c9209
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-10-31 10:50:03 +0300

    16. Optional Styling

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -1,56 +1,59 @@
-import React from 'react';
-import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import Gift from './Gift';
+import React from "react";
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Gift from "./Gift";
 
 configure({ adapter: new Adapter() });
 
-describe('Gift', () => {
-    const mockRemove = jest.fn();
-    const id = 1;
-    const props = { gift: {id}, removeGift: mockRemove};
-    const gift = shallow(<Gift {...props} />);
+describe("Gift", () => {
+  const mockRemove = jest.fn();
+  const id = 1;
+  const props = { gift: { id }, removeGift: mockRemove };
+  const gift = shallow(<Gift {...props} />);
 
-    it('renders properly', () => {
-        expect(gift).toMatchSnapshot();
+  it("renders properly", () => {
+    expect(gift).toMatchSnapshot();
+  });
+
+  it("initializes a person and present in `state`", () => {
+    expect(gift.state()).toEqual({ person: "", present: "" });
+  });
+
+  describe("when typing into the person input", () => {
+    const person = "Piper";
+
+    beforeEach(() => {
+      gift
+        .find(".input-person")
+        .simulate("change", { target: { value: person } });
     });
 
-    it('initializes a person and present in `state`', () => {
-       expect(gift.state()).toEqual({ person: '', present: '' });
+    it("updates the person in `state`", () => {
+      expect(gift.state().person).toEqual(person);
+    });
+  });
+
+  describe("when typein into the present input", () => {
+    const present = "Dress";
+
+    beforeEach(() => {
+      gift
+        .find(".input-present")
+        .simulate("change", { target: { value: present } });
     });
 
-    describe('when typing into the person input', () => {
-        const person = 'Piper';
+    it("update the present in `state`", () => {
+      expect(gift.state().present).toEqual(present);
+    });
+  });
 
-        beforeEach(() => {
-            gift.find('.input-person').simulate('change', { target: { value: person} });
-        });
-
-        it('updates the person in `state`', () => {
-            expect(gift.state().person).toEqual(person);
-        });
+  describe("when clicking the `Remove Gift` button", () => {
+    beforeEach(() => {
+      gift.find(".btn-remove").simulate("click");
     });
 
-    describe('when typein into the present input', () => {
-       const present = 'Dress';
-
-       beforeEach(() => {
-           gift.find('.input-present').simulate('change', { target: { value: present } });
-       });
-
-       it('update the present in `state`', () => {
-           expect(gift.state().present).toEqual(present);
-       });
+    it("calls the removeGift callback", () => {
+      expect(mockRemove).toHaveBeenCalledWith(id);
     });
-
-    describe('when clicking the `Remove Gift` button', () => {
-        beforeEach(() => {
-           gift.find('.btn-remove').simulate('click');
-        });
-
-        it('calls the removeGift callback', () => {
-            expect(mockRemove).toHaveBeenCalledWith(id);
-        });
-    });
-
+  });
 });
