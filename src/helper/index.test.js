f04e70be023a5121cb3dc3fb5aef9a933c1459c5
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2018-10-31 06:55:54 +0300

    Add Prettier To WebStorm

--- src/helper/index.test.js
+++ src/helper/index.test.js
@@ -1,15 +1,15 @@
-import { max_number } from './index';
+import { max_number } from "./index";
 
-describe('max_number', () => {
-    describe('given an empty array', () => {
-        it('returns 0', () => {
-            expect(max_number([])).toEqual(0);
-        });
+describe("max_number", () => {
+  describe("given an empty array", () => {
+    it("returns 0", () => {
+      expect(max_number([])).toEqual(0);
     });
+  });
 
-    describe('given an array of numbers', () => {
-        it('returns the max number', () => {
-            expect(max_number([1, 2, 3])).toEqual(3)
-        })
-    })
+  describe("given an array of numbers", () => {
+    it("returns the max number", () => {
+      expect(max_number([1, 2, 3])).toEqual(3);
+    });
+  });
 });
