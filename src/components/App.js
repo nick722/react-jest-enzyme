da1cd13b41e264fea70784dae15916b99e1be898
Nikolai_Iakushchenko <nikolai_iakushchenko@epam.com>
2018-10-31 11:16:43 +0300

    18. Start Up LootCheck with an App Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,10 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
-import Gift from "./Gift";
-import { max_number } from "../helper";
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = { gifts: [] };
-  }
-
-  addGift = () => {
-    const { gifts } = this.state;
-
-    gifts.push({ id: max_number(this.state.gifts.map(gift => gift.id)) + 1 });
-
-    this.setState({ gifts });
-  };
-
-  removeGift = id => {
-    const gifts = this.state.gifts.filter(gift => gift.id !== id);
-
-    this.setState({ gifts });
-  };
-
   render() {
     return (
       <div>
-        <h2>Gift Giver</h2>
-        <div className="gift-list">
-          {this.state.gifts.map(gift => {
-            return (
-              <Gift key={gift.id} gift={gift} removeGift={this.removeGift} />
-            );
-          })}
-        </div>
-        <Button onClick={this.addGift} className="btn-add">
-          Add Gift
-        </Button>
+        <h2>Loot Check</h2>
       </div>
     );
   }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Adapter from "enzyme-adapter-react-16";
 import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 import App from "./App";
 
@@ -8,45 +8,8 @@ configure({ adapter: new Adapter() });
 
 describe("App", () => {
   const app = shallow(<App />);
-  it("renders correctly", () => {
+
+  it("renders properly", () => {
     expect(app).toMatchSnapshot();
-  });
-
-  it("initializes the `state` with an empty list of gifts", () => {
-    expect(app.state().gifts).toEqual([]);
-  });
-
-  describe("when clicking the `add-gift` button", () => {
-    const id = 1;
-
-    beforeEach(() => {
-      app.find(".btn-add").simulate("click");
-    });
-
-    afterEach(() => {
-      app.setState({ gifts: [] });
-    });
-
-    it("adds a new gift to `state`", () => {
-      expect(app.state().gifts).toEqual([{ id }]);
-    });
-
-    it("adds a new gift to the rendered list", () => {
-      expect(app.find(".gift-list").children().length).toEqual(1);
-    });
-
-    it("creates a Gift component", () => {
-      expect(app.find("Gift").exists()).toBe(true);
-    });
-
-    describe("and the user wants to remove the add gift", () => {
-      beforeEach(() => {
-        app.instance().removeGift(id);
-      });
-
-      it("removes the gift from `state`", () => {
-        expect(app.state().gifts).toEqual([]);
-      });
-    });
   });
 });
